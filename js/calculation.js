78595c7c67598bdaabff0081ee2c04c398cf0514
Alauddin33 <shamem33@gmail.com>
2022-08-23 11:48:47 +0600

    js calculation added

---
+++ js/calculation.js
@@ -0,0 +1,50 @@
+// playername display section:
+
+const playerArray = [];
+
+function display(playerName) {
+    const selectedPlayerName = document.getElementById('selected-player-name');
+    selectedPlayerName.innerHTML = "";
+    for (let i = 0; i < playerName.length; i++) {
+        const name = playerArray[i].playerName;
+        const li = document.createElement('li');
+        li.innerText = name;
+        selectedPlayerName.appendChild(li);
+    }
+
+}
+
+
+
+// player expense calculation:
+
+function count(number) {
+    const playerNumber = number.length;
+    document.getElementById('calculate-btn').addEventListener('click', function () {
+        const elementValue = getElementNumber('per-player-expenses')
+        const playerExpense = elementValue * playerNumber;
+        const playerExpenseTotal = document.getElementById('player-expenses');
+        playerExpenseTotal.innerText = playerExpense;
+    })
+
+}
+
+
+
+
+// grand total calculation
+
+document.getElementById('calculate-total-btn').addEventListener('click', function () {
+    const playerExpenseValue = getElementNumber('player-expenses')
+    const managerExpense = getElementNumber('manager-expense');
+    const coachExpense = getElementNumber('coach-expense');
+    const grandtotal = document.getElementById("grand-total");
+    grandtotal.innerText = playerExpenseValue + managerExpense + coachExpense;
+
+})
+
+
+// done
+// done
+// done
+// done
--- js/common.js
+++ js/common.js
@@ -1,17 +1,4 @@
-// playername display section:
-const playerArray = [];
 
-function display(playerName) {
-    const selectedPlayerName = document.getElementById('selected-player-name');
-    selectedPlayerName.innerHTML = "";
-    for (let i = 0; i < playerName.length; i++) {
-        const name = playerArray[i].playerName;
-        const li = document.createElement('li');
-        li.innerText = name;
-        selectedPlayerName.appendChild(li);
-    }
-
-}
 
 // playername selection:
 
@@ -22,30 +9,33 @@ function addToList(element) {
         playerName: playerName
     }
     playerArray.push(playerObj);
-    // console.log(playerArray);
-    // console.log(playerArray.length);
+
     if (playerArray.length > 5) {
         alert('You cannot add more than five!!!')
         return;
     }
     display(playerArray);
     count(playerArray);
+
     const playerExpenseTotal = document.getElementById('player-expenses');
     playerExpenseTotal.innerText = "";
+    const grandtotal = document.getElementById("grand-total");
+    grandtotal.innerText = "";
 }
 
-// player expense calculation:
-function count(number) {
-    const playerNumber = number.length;
-    document.getElementById('calculate-btn').addEventListener('click', function () {
-        const perPlayerExpense = document.getElementById('per-player-expenses');
-        const perPlayerExpenseString = perPlayerExpense.innerText;
-        const perPlayerExpenseValue = parseInt(perPlayerExpenseString);
-        const playerExpense = perPlayerExpenseValue * playerNumber;
-        const playerExpenseTotal = document.getElementById('player-expenses');
-        playerExpenseTotal.innerText = playerExpense;
-    })
+// common get element function
+function getElementNumber(elementId) {
+    const element = document.getElementById(elementId);
+    const elementString = element.innerText;
+    const elementValue = parseInt(elementString);
+    return elementValue;
 }
+
+
+
+
+
+
 
 
 
